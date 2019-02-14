4d1f3d6c8988ee7e4221d25922fc239c4e089ff8
Alexander Handstanger <zombeyfan.ah@gmail.com>
2019-02-14 08:54:50 +0100

    finish

--- merge.c
+++ merge.c
@@ -45,4 +45,28 @@ void print_array(int* array, int n)
 
 void merge(int* first_array, int n_first, int* second_array, int n_second, int* merged_array)
 {
+  //404 Error(exaption_tooTired);
+  int i = 0;
+  int j = 0;
+  int k = 0;
+
+
+  while (i<n_first && j <n_second){
+      if (first_array[i] < second_array[j]){
+          merged_array[k++] = first_array[i++];
+      }
+      else{
+          merged_array[k++] = second_array[j++];
+      }
+  }
+  while (i < n_first){
+    if (n_first > 0){
+      merged_array[k++] = first_array[i++];
+    }
+  }
+  while (j < n_second){
+    if(n_second > 0){
+      merged_array[k++] = second_array[j++];
+    }
+  }
 }
